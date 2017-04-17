f51f2e549a8db94e26745c32b2c9145a053d4b41
kevintyll <kevintyll@gmail.com>
2017-04-17 14:47:48 -0400

    Add git_push to ignore.

--- git_push.sh
+++ git_push.sh
@@ -26,29 +26,11 @@ if [ "$release_note" = "" ]; then
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
-# Initialize the local directory as a Git repository
-#git init
-
 # Adds the files in the local repository and stages them for commit.
 git add .
 
 # Commits the tracked changes and prepares them to be pushed to a remote repository.
 git commit -m "$release_note"
-
-# Sets the new remote
-#git_remote=`git remote`
-#if [ "$git_remote" = "" ]; then # git remote not defined
-#
-#    if [ "$GIT_TOKEN" = "" ]; then
-#        echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git crediential in your environment."
-#        git remote add origin https://github.com/${git_user_id}/${git_repo_id}.git
-#    else
-#        git remote add origin https://${git_user_id}:${GIT_TOKEN}@github.com/${git_user_id}/${git_repo_id}.git
-#    fi
-#
-#fi
-
-#git pull origin master
 
 # Pushes (Forces) the changes in the local repository up to the remote repository
 echo "Git pushing to https://github.com/${git_user_id}/${git_repo_id}.git"
